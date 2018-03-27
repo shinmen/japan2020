11824722b03204979751cfc853e98bb548167e4c
julien.bouffard <julien.bouffard@link-value.fr>
2018-03-27 16:27:07 +0200

    implements flight offers save on flight request

--- src/Infrastructure/Flight/Consumer/FlightOffersConsumer.php
+++ src/Infrastructure/Flight/Consumer/FlightOffersConsumer.php
@@ -2,17 +2,36 @@
 
 namespace App\Infrastructure\Flight\Consumer;
 
+use MongoDB\Client;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 
 final class FlightOffersConsumer implements ConsumerInterface
 {
     const NACK = false;
+    const ACK = true;
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        $this->client->selectDatabase('japan2020');
+    }
     
     public function execute(AMQPMessage $msg)
     {
-	$flightPlan = unserialize($msg->getBody());
+        try {
+            $flightPlan = unserialize($msg->getBody());
+            $json = json_encode($flightPlan);
+            $collection = $this->client->japan2020->flights_fares;
+            $collection->insertOne(json_decode($json, true));
 
-        return self::NACK;
+            return self::ACK;
+        } catch(\Exception $e) {
+            return self::NACK;
+        }
     }
 }
